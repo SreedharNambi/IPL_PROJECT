7ce24c385654b690443a0137b4e90bdaac0a40c2
Sreedhar Nambi <Sreedharan1997@gmail.com>
2022-05-20 18:26:39 +0530

    Reading data from line2 in while loopOmitted remove method.

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -6,21 +6,26 @@ public class CSVReader {
     public static void main (String [] args){
         String path="/home/sreedhar/IdeaProjects/Project1/src/matches.csv";
         String line="";
+//        Scenario 1 :- No. of matches played in a year by all teams
         HashMap <String,Integer> requiredMap=new HashMap<String,Integer>();
+//        Scenario 2:-
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
+            line=br.readLine();
             while((line=br.readLine())!=null){
                 String [] values=line.split(",");
                 int count= requiredMap.containsKey(values[1])? requiredMap.get(values[1]):0;
                 requiredMap.put(values[1],count+1 );
             }
-            requiredMap.remove("season");
+//            requiredMap.remove("season");
             System.out.println(requiredMap);
 
             }catch (IOException e){
             e.printStackTrace();
+
         }
 
         }
     }
+
 
