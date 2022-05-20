841ec8438dba6c19740878e9661938bf02986c42
Sreedhar Nambi <Sreedharan1997@gmail.com>
2022-05-20 15:14:36 +0530

    Generating unique keys

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,18 +1,23 @@
 import java.io.*;
+import java.util.TreeSet;
 
 public class CSVReader {
     public static void main (String [] args){
         String path="/home/sreedhar/IdeaProjects/Project1/src/matches.csv";
         String line="";
-
+        TreeSet<String> season=new TreeSet<String>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             while((line=br.readLine())!=null){
                 String [] values=line.split(",");
-                System.out.println("Match Id: "+values[0]+", Season: "+values[1]+", Winner: "+values[10]);
+                season.add(values[1]);
             }
-        } catch (IOException e){
+            season.remove("season");
+            System.out.println(season);
+            }catch (IOException e){
             e.printStackTrace();
         }
+
+        }
     }
-}
+
