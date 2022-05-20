01933b982a2ac14e0a094963e7dc5d7dd4c9095a
Sreedhar Nambi <Sreedharan1997@gmail.com>
2022-05-20 16:51:46 +0530

    Created a hashmap and used a ternary operator to add keys and values

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -1,19 +1,22 @@
 import java.io.*;
+import java.util.HashMap;
 import java.util.TreeSet;
 
 public class CSVReader {
     public static void main (String [] args){
         String path="/home/sreedhar/IdeaProjects/Project1/src/matches.csv";
         String line="";
-        TreeSet<String> season=new TreeSet<String>();
+        HashMap <String,Integer> requiredMap=new HashMap<String,Integer>();
         try {
             BufferedReader br = new BufferedReader(new FileReader(path));
             while((line=br.readLine())!=null){
                 String [] values=line.split(",");
-                season.add(values[1]);
+                int count= requiredMap.containsKey(values[1])? requiredMap.get(values[1]):0;
+                requiredMap.put(values[1],count+1 );
             }
-            season.remove("season");
-            System.out.println(season);
+            requiredMap.remove("season");
+            System.out.println(requiredMap);
+
             }catch (IOException e){
             e.printStackTrace();
         }
